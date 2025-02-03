0cc17daae9a0d9f15e8644ea837e6c4ea44da2f5
Rohit <dv6510@gmail.com>
2025-02-03 11:33:03 +0530

    done

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,9 +5,13 @@ const User = require('../models/userModel')
 const signup = async(req, res)=>{
     try {
         const {username, email, password, DOB} = req.body;
-        if(([username, email, password, DOB].some((el)=> el.trime()= ""))){
-            return res.status(401).json({err: "All fields are required"})
-        } 
+        if(!username) return res.status(401).json({err:"Username cannot be empty"})
+        if(!email) return res.status(401).json({err:"Email cannot be empty"})
+        if(!password) return res.status(401).json({err:"Password cannot be empty"})
+        if(password.length<8 || password.length> 16){
+            return res.status(401).json({err: "Password cannot be short than 8 character or longer than 16 characters"})
+        }
+        
 
         const user = new User({username, email, password, DOB})
         await user.save()
